c4648b3de703d9c7bfaee8bca31bbec08f5177b4
Jose Espejo <jespagu2610@g.educaand.es>
2025-03-31 19:32:25 +0200

    Commit Final8

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log("Running server...");
 const express = require("express");
 const cors = require("cors");
 const admin = require("firebase-admin");
-var serviceAccount = require(sianwebsite-firebase-adminsdk-fbsvc-a0e15a8d2f.json);
+const serviceAccount = require("sianwebsite-firebase-adminsdk-fbsvc-a0e15a8d2f.json");
 
 // Inicializa Firebase Admin SDK // Asegúrate de que esta ruta sea correcta
 
